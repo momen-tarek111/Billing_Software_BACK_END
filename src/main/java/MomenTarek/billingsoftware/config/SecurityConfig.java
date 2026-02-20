142dc1bd7958b8337f88a0b853b69955d0265bba
momen-tarek111 <momen.tarek.nagaty@gmail.com>
2026-02-21 00:50:21 +0200

    Add Front_URL

--- src/main/java/MomenTarek/billingsoftware/config/SecurityConfig.java
+++ src/main/java/MomenTarek/billingsoftware/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     }
     private UrlBasedCorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://billingsoftware-beta.vercel.app"));
         config.setAllowedMethods(List.of("GET","POST","DELETE","PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization","Content-Type"));
         config.setAllowCredentials(true);
